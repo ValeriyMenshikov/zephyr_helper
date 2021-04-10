55e9c1c16ccf18aaf05cd36a0663c8531e5b1314
vmenshikov <vmenshikov@ozon.ru>
2021-04-10 23:09:24 +0300

    создал константы

---
+++ constants.py
@@ -0,0 +1,21 @@
+class TestCaseStatus:
+    DRAFT = 733
+    APPROVED = 735
+    DEPRECATED = 734
+    MANUAL = 1623
+    AUTOMATED = 1625
+    READY_TO_AUTO = 1624
+
+
+class TestCaseResult:
+    NOT_EXECUTED = 728
+    IN_PROGRESS = 729
+    PASS = 730
+    FAIL = 731
+    BLOCKED = 732
+
+
+class TestCasePriority:
+    HIGH = 1401
+    NORMAL = 1401
+    LOW = 1401
